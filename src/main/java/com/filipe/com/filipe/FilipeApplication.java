60fb47ccabed2ae29ea0459379c3c05c58b81457
filipebraga <fbragasoftware@gmail.com>
2024-12-07 10:19:53 -0300

    first commit

---
+++ src/main/java/com/filipe/com/filipe/FilipeApplication.java
@@ -0,0 +1,13 @@
+package com.filipe.com.filipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipe/com/filipe/FilipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe.com.filipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
